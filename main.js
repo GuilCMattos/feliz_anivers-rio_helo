16b25f3d6659916bec8245affb288325ad85f3a9
Guilherme Mattos <91619478+GuilCMattos@users.noreply.github.com>
2022-04-05 17:35:01 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+function firstGift() {
+  document.getElementById('firstSession').style.display = 'block'
+  document.getElementById('secondSession').style.display = 'none'
+  document.getElementById('thirdSession').style.display = 'none'
+  document.getElementById('forthSession').style.display = 'none'
+
+ 
+}
+
+function secondGift() {
+  document.getElementById('firstSession').style.display = 'none'
+  document.getElementById('secondSession').style.display = 'block'
+  document.getElementById('thirdSession').style.display = 'none'
+  document.getElementById('forthSession').style.display = 'none'
+
+ 
+}
+
+function thirdGift() {
+  document.getElementById('firstSession').style.display = 'none'
+  document.getElementById('secondSession').style.display = 'none'
+  document.getElementById('thirdSession').style.display = 'block'
+  document.getElementById('forthSession').style.display = 'none'
+ 
+ 
+}
+
+function fourthGift() {
+  document.getElementById('firstSession').style.display = 'none'
+  document.getElementById('secondSession').style.display = 'none'
+  document.getElementById('thirdSession').style.display = 'none'
+  document.getElementById('forthSession').style.display = 'block'
+  
+ 
+}
+
